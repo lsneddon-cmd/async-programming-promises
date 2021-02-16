ebff18ff85e9485b022ba00efec44898b819ea56
Lewis Sneddon <>
2021-02-16 09:37:22 +0000

    parallel

--- src/iterating.mjs
+++ src/iterating.mjs
@@ -35,7 +35,19 @@ export async function concurrent(){
   appendText(JSON.stringify(order[0]));
 }
 
-export function parallel(){
+export async function parallel(){
+  setText("");
+
+  await Promise.all([
+    (async () => {
+      const {data} = await axios.get("http://localhost:3000/orderStatuses");
+      appendText(JSON.stringify(data));
+    }) (),
+    (async () => {
+      const {data} = await axios.get("http://localhost:3000/orders");
+      appendText(JSON.stringify(data));
+    }) ()
+  ]);
 }
 
 
